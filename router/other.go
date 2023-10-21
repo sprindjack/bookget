428986535b666524786e365882bf1371ec17193f
zhudw <zhudwi@foxmail.com>
2023-10-21 20:50:54 +0800

    [update] iiif.io

--- router/other.go
+++ router/other.go
@@ -57,11 +57,11 @@ func (p Sillokgokr) getRouterInit(sUrl []string) (map[string]interface{}, error)
 type DlibGoKr struct{}
 
 func (p DlibGoKr) getRouterInit(sUrl []string) (map[string]interface{}, error) {
-	for i, s := range sUrl {
-		n := i + 1
-		var libgokr app.DlibGoKr
-		libgokr.Init(n, s)
-	}
+	//for i, s := range sUrl {
+	//	n := i + 1
+	//	var libgokr app.DlibGoKr
+	//	libgokr.Init(n, s)
+	//}
 	return nil, nil
 }
 
