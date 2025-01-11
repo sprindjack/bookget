3423bc1d23f7451f39e5c245a44abb4b50ec294a
zhudw <zhudwi@outlook.com>
2025-01-11 16:49:58 +0800

    [fixed]国图多册

--- app/nlc.go
+++ app/nlc.go
@@ -208,13 +208,9 @@ func (r *ChinaNlc) getVolumes(sUrl string, jar *cookiejar.Jar) (volumes []string
 	}
 	text := util.SubText(string(r.body), "<div id=\"multiple\"", "id=\"catalogDiv\">")
 	//取册数
-	aUrls := regexp.MustCompile(`<a[^>]+class="a1"[^>].+href="/OutOpenBook/([^"]+)"`).FindAllStringSubmatch(text, -1)
-	if aUrls == nil {
-		//http://read.nlc.cn/menhu/allSearch/searchDetail?searchType=110&showType=1&indexName=data_040&fid=MU2024005
-		aUrls = regexp.MustCompile(`<a[^>]+class="a1"[^>].+href="/menhu/OutOpenBook/([^"]+)"`).FindAllStringSubmatch(text, -1)
-	}
+	aUrls := regexp.MustCompile(`<a[^>]+class="a1"[^>].+href="([^"]+)OutOpenBook/([^"]+)"`).FindAllStringSubmatch(text, -1)
 	for _, uri := range aUrls {
-		pageUrl := fmt.Sprintf("%s://%s/OutOpenBook/%s", r.dt.UrlParsed.Scheme, r.dt.UrlParsed.Host, uri[1])
+		pageUrl := fmt.Sprintf("%s://%s%sOutOpenBook/%s", r.dt.UrlParsed.Scheme, r.dt.UrlParsed.Host, uri[1], uri[2])
 		volumes = append(volumes, pageUrl)
 	}
 	//
